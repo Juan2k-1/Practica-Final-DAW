fc8d063366ab4095cd4b64a1e755c2ff7407cedc
Juan2k-1 <jadova16@gmail.com>
2023-10-13 17:22:37 +0200

    Añadida más funcionalidad javascript a la web para las imagenes

---
+++ src/main/webapp/js/Script_ClickImages.js
@@ -0,0 +1,27 @@
+// Obtén las imágenes por su ID
+var imagenCinqueTerre = document.getElementById("CinqueTerre");
+var imagenNewYork = document.getElementById("NewYork");
+var imagenSanFrancisco = document.getElementById("SanFrancisco");
+var imagenPisa = document.getElementById("Pisa");
+var imagenParis = document.getElementById("Paris");
+
+// Agrega eventos de clic a las imágenes
+imagenCinqueTerre.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+imagenNewYork.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+imagenSanFrancisco.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+imagenPisa.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+imagenParis.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
