102fb847b3dc17b62e0a3970c7b9ff5405b8fb32
Juan2k-1 <jadova16@gmail.com>
2023-10-20 16:36:19 +0200

    Añadida contenido RWD

--- src/main/webapp/js/Script_ClickImages.js
+++ src/main/webapp/js/Script_ClickImages.js
@@ -1,27 +1,27 @@
-// Obtén las imágenes por su ID
+// Obtener los nombres de las imágenes por su ID
 var imagenCinqueTerre = document.getElementById("CinqueTerre");
 var imagenNewYork = document.getElementById("NewYork");
 var imagenSanFrancisco = document.getElementById("SanFrancisco");
 var imagenPisa = document.getElementById("Pisa");
 var imagenParis = document.getElementById("Paris");
 
-// Agrega eventos de clic a las imágenes
-imagenCinqueTerre.addEventListener("click", function() {
+// Agrega eventos de clic a los nombres de las imágenes
+imagenCinqueTerre.addEventListener("click", function () {
     window.location.href = "index.html";
 });
 
-imagenNewYork.addEventListener("click", function() {
+imagenNewYork.addEventListener("click", function () {
     window.location.href = "index.html";
 });
 
-imagenSanFrancisco.addEventListener("click", function() {
+imagenSanFrancisco.addEventListener("click", function () {
     window.location.href = "index.html";
 });
 
-imagenPisa.addEventListener("click", function() {
+imagenPisa.addEventListener("click", function () {
     window.location.href = "index.html";
 });
 
-imagenParis.addEventListener("click", function() {
+imagenParis.addEventListener("click", function () {
     window.location.href = "index.html";
 });
